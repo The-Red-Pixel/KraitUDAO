7748061cb913311b3b6d1c37e72c395a24ff6e9a
Kumonda221 <kumonda@foxmail.com>
2018-02-06 14:05:56 +0800

    增量更新 20180206

--- src/org/kucro3/kraitudao/DataSource.java
+++ src/org/kucro3/kraitudao/DataSource.java
@@ -1,5 +1,18 @@
 package org.kucro3.kraitudao;
 
+@SuppressWarnings("unchecked")
 public interface DataSource {
-    
+    public default void query(Object object) throws DataSourceException
+    {
+        query(object, (Class) object.getClass());
+    }
+
+    public <T> void query(T object, Class<T> type) throws DataSourceException;
+
+    public default void commit(Object object) throws DataSourceException
+    {
+        commit(object, (Class) object.getClass());
+    }
+
+    public <T> void commit(T object, Class<T> type) throws DataSourceException;
 }
