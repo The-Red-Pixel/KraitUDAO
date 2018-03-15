34689e47200dd38e6946469acaca3256ad07c647
Kumonda221 <kumonda@trp.fun>
2018-03-15 22:42:03 +0800

    增量更新 20180315

--- src/com/theredpixelteam/kraitudao/interpreter/SimpleDataObjectInterpreter.java
+++ src/com/theredpixelteam/kraitudao/interpreter/SimpleDataObjectInterpreter.java
@@ -1,6 +1,5 @@
 package com.theredpixelteam.kraitudao.interpreter;
 
-import com.sun.org.apache.xpath.internal.operations.Mult;
 import com.theredpixelteam.kraitudao.dataobject.*;
 
 import java.util.*;
@@ -27,15 +26,17 @@ public class SimpleDataObjectInterpreter implements DataObjectInterpreter {
 
     private static class UniqueDataObjectContainer implements UniqueDataObject
     {
-        UniqueDataObjectContainer(Class<?> type, ValueObject key)
+        UniqueDataObjectContainer(Class<?> type)
         {
             this.data = new HashMap<>();
             this.type = type;
-            this.key = key;
         }
 
         void seal()
         {
+            if(this.key == null)
+                throw new DataObjectMalformationException("Key not defined");
+
             this.data = Collections.unmodifiableMap(data);
         }
 
@@ -72,43 +73,56 @@ public class SimpleDataObjectInterpreter implements DataObjectInterpreter {
 
     private static class MultipleDataObjectContainer implements MultipleDataObject
     {
-        MultipleDataObjectContainer()
+        MultipleDataObjectContainer(Class<?> type)
         {
+            this.type = type;
+        }
 
+        void seal()
+        {
+            if(this.primaryKey == null)
+                throw new DataObjectMalformationException("Primary Key not defined");
+
+            this.secondaryKeys = Collections.unmodifiableMap(secondaryKeys);
+            this.values = Collections.unmodifiableMap(values);
         }
 
         @Override
         public ValueObject getPrimaryKey()
         {
-            return null;
+            return primaryKey;
         }
 
         @Override
         public Map<String, ValueObject> getSecondaryKeys()
         {
-            return null;
+            return secondaryKeys;
         }
 
         @Override
         public Optional<ValueObject> getValue(String name)
         {
-            return Optional.empty();
+            return Optional.ofNullable(values.get(name));
         }
 
         @Override
         public Map<String, ValueObject> getValues()
         {
-            return null;
+            return values;
         }
 
         @Override
         public Class<?> getType()
         {
-            return null;
+            return type;
         }
 
         final Class<?> type;
 
+        ValueObject primaryKey;
 
+        Map<String, ValueObject> secondaryKeys;
+
+        Map<String, ValueObject> values;
     }
 }
