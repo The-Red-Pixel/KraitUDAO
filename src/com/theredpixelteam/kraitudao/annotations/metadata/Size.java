88c5fcfb3cb6b8510032cac9af5dff8b46042835
Kumonda221 <kumonda@trp.fun>
2018-05-12 22:09:44 +0800

    增量更新 20180512

--- src/com/theredpixelteam/kraitudao/annotations/metadata/Size.java
+++ src/com/theredpixelteam/kraitudao/annotations/metadata/Size.java
@@ -1,16 +1,21 @@
 package com.theredpixelteam.kraitudao.annotations.metadata;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Metadata
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.TYPE})
+@Repeatable(Size.RepeatableSize.class)
 public @interface Size {
     @ExpandedName
     String name() default "";
 
     int n();
+
+    @MetadataCollection(Size.class)
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.FIELD, ElementType.TYPE})
+    @interface RepeatableSize {
+        Size[] value();
+    }
 }
