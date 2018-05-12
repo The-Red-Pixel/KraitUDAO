dcdd10a0e7529dbf40ef5805c0b858dbfab3be70
Kumonda221 <kumonda@trp.fun>
2018-05-12 09:23:11 +0800

    增量更新 20180512

--- src/com/theredpixelteam/kraitudao/common/PlainDatabaseDataSource.java
+++ src/com/theredpixelteam/kraitudao/common/PlainDatabaseDataSource.java
@@ -7,16 +7,24 @@ import com.theredpixelteam.kraitudao.dataobject.DataObject;
 import com.theredpixelteam.kraitudao.interpreter.DataObjectInterpreter;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PlainDatabaseDataSource implements DataSource {
     public PlainDatabaseDataSource(Connection connection, String tableName, DataObjectInterpreter interpreter)
+            throws DataSourceException
     {
         this.connection = connection;
         this.tableName = tableName;
         this.interpreter = interpreter;
+
+        try {
+            this.connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            throw new DataSourceException(e);
+        }
     }
 
     public Connection getConnection()
@@ -102,4 +110,22 @@ public class PlainDatabaseDataSource implements DataSource {
     protected DataObjectInterpreter interpreter;
 
     protected static final Map<Class<?>, DataObject> cache = new HashMap<>();
+
+    private static final Map<Class<?>, Class<?>> BOXING = new HashMap<Class<?>, Class<?>>() {
+        {
+            put(Boolean.class, boolean.class);
+            put(Character.class, char.class);
+            put(Short.class, short.class);
+            put(Integer.class, int.class);
+            put(Long.class, long.class);
+            put(Float.class, float.class);
+            put(Double.class, double.class);
+        }
+    };
+
+    private static final Map<Class<?>, String> MAPPING = new HashMap<Class<?>, String>() {
+        {
+
+        }
+    };
 }
