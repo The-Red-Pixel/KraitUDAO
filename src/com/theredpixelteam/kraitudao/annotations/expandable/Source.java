037827deaa1852e447177cfbeec0d68a763a786c
Kumonda221 <kumonda@trp.fun>
2018-06-07 12:38:19 +0800

    更新版权信息 20180607

--- src/com/theredpixelteam/kraitudao/annotations/expandable/Source.java
+++ src/com/theredpixelteam/kraitudao/annotations/expandable/Source.java
@@ -1,14 +1,27 @@
+/*
+ * Source.java
+ *
+ * Copyright (C) 2018 The Red Pixel <theredpixelteam.com>
+ * Copyright (C) 2018 KuCrO3 Studio <kucro3.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
 package com.theredpixelteam.kraitudao.annotations.expandable;
 
-/**
- * 版权所有（C） The Red Pixel <theredpixelteam.com>
- * 版权所有（C)  KuCrO3 Studio
- * 这一程序是自由软件，你可以遵照自由软件基金会出版的GNU通用公共许可证条款
- * 来修改和重新发布这一程序。或者用许可证的第二版，或者（根据你的选择）用任
- * 何更新的版本。
- * 发布这一程序的目的是希望它有用，但没有任何担保。甚至没有适合特定目的的隐
- * 含的担保。更详细的情况请参阅GNU通用公共许可证。
- */
 public enum Source {
     THIS,
     FIELD
